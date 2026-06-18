93217fa6a7b8582dcb70c2986903fc9e99612d60
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2026-06-18 17:49:21 +0200

    🔐 refactor: secure auth and type session context

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,3 +1,4 @@
+import "server-only";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { betterAuth } from "better-auth/minimal";
 import { createContext, type MiddlewareFunction } from "react-router";
@@ -9,23 +10,44 @@ export const auth = betterAuth({
   }),
   socialProviders: {
     discord: {
-      clientId: process.env.DISCORD_CLIENT_ID!,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      clientId: import.meta.env.VITE_DISCORD_CLIENT_ID!,
+      clientSecret: import.meta.env.VITE_DISCORD_CLIENT_SECRET!,
     },
   },
 });
 
 export const Auth = createContext<typeof auth>();
-export const Session = createContext();
+
+export interface Session {
+  session: {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    userId: string;
+    expiresAt: Date;
+    token: string;
+    ipAddress?: string | null | undefined;
+    userAgent?: string | null | undefined;
+  };
+  user: {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    email: string;
+    emailVerified: boolean;
+    name: string;
+    image?: string | null | undefined;
+  };
+}
+
+export const SESSION = createContext<Promise<Session | null>>();
 
 export const sessionMiddleware: MiddlewareFunction = async (args, next) => {
   args.context.set(Auth, auth);
-  const session = await auth.api.getSession({
+  const session = auth.api.getSession({
     headers: args.request.headers,
   });
-  args.context.set(Session, session);
-
-  session?.user.id;
+  args.context.set(SESSION, session);
 
   return next();
 };
