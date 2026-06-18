b8e57936535ff723af46c453beabe3c21ef10665
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2026-06-18 17:49:01 +0200

    🔧 Sanitize branch name for Cloudflare deploy

--- app/db/middleware.ts
+++ app/db/middleware.ts
@@ -1,10 +1,13 @@
+import "server-only";
 import { env } from "cloudflare:workers";
 import { drizzle } from "drizzle-orm/d1";
 import { createContext, type MiddlewareFunction } from "react-router";
 import { relations } from "./relations";
 import * as schema from "./schema";
 
-export const Db = createContext<ReturnType<typeof drizzle<typeof schema, typeof relations>>>();
+export type Db = ReturnType<typeof drizzle<typeof schema, typeof relations>>;
+
+export const DB = createContext<Db>();
 
 export const db = drizzle(env.DB, {
   relations,
@@ -12,6 +15,6 @@ export const db = drizzle(env.DB, {
 });
 
 export const dbMiddleware: MiddlewareFunction = async (args, next) => {
-  args.context.set(Db, db);
+  args.context.set(DB, db);
   return next();
 };
